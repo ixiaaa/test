c7a107c40a0967afb751d460c7fd8994ac5fc0e7
Irena Xia <ixia@Irenas-MacBook-Pro.local>
2016-12-01 23:16:37 -0500

    update

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-$("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".title").offset().top},
-        'slow');
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+$(function(){
+ var shrinkHeader = 150;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+      if ( scroll >= shrinkHeader ) {
+           $('.header').addClass('shrink');
+        }
+        else {
+            $('.header').removeClass('shrink');
+        }
+  });
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+    }
 });
