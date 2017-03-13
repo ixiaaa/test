5a888b20b56fe80451048b1062a067164851059f
Irena Xia <ixia@Irenas-MacBook-Pro.local>
2017-03-12 22:23:41 -0400

    y

--- main.js
+++ main.js
@@ -20,3 +20,19 @@ $('#return-to-top').click(function() {      // When arrow is clicked
         scrollTop : 450                       // Scroll to top of body
     }, 500);
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
